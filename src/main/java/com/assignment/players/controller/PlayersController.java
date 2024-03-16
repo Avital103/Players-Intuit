2737d034b9dccd9de90095791460b776be8ce4a8
aarvivo <aarvivo@synamedia.com>
2024-03-16 19:49:57 +0200

    change get by player id

--- src/main/java/com/assignment/players/controller/PlayersController.java
+++ src/main/java/com/assignment/players/controller/PlayersController.java
@@ -7,11 +7,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @RestController
 @Api("Get Players APIs")
@@ -27,13 +30,21 @@ public class PlayersController {
     @GetMapping()
     public List<Player> getPlayers() throws Exception {
         Path path = Paths.get("/Users/aarvivo/Documents/player.csv");
-        List<Player> players = new ArrayList();
+        List<Player> players = new ArrayList<>();
         return playerService.readCsvToBeanList(path, Player.class, players);
     }
 
     @GetMapping("/{playerID}")
-    public String getPlayerById(@PathVariable String playerID) {
-        return playerID;
+    public Player getPlayerById(@PathVariable String playerID) throws Exception {
+        Path path = Paths.get("/Users/aarvivo/Documents/player.csv");
+        List<Player> players = new ArrayList<>();
+        List<Player> allPlayers = playerService.readCsvToBeanList(path, Player.class, players);
+        for (Player player : allPlayers) {
+            if (player.getPlayerID().equals(playerID)) {
+                return player;
+            }
+        }
+        throw new ResponseStatusException(NOT_FOUND, "Unable to find player");
     }
 
 }
