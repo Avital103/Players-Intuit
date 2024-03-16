ca208ff4368e74610396dc78532f315695582265
aarvivo <aarvivo@synamedia.com>
2024-03-16 21:12:20 +0200

    change unit test to call configureMocks

--- src/test/java/com/assignment/players/PlayersApplicationTests.java
+++ src/test/java/com/assignment/players/PlayersApplicationTests.java
@@ -15,6 +15,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -33,52 +34,70 @@ class PlayersControllerTest {
         MockitoAnnotations.openMocks(this);
     }
 
-    Player player1 = new Player("player1", 1877, 4, 15, "USA", "PA", "Latrobe", 1957,
-            1, 6, "USA", "FL", "Fort Lauderdale",
-            "Ed", "Abbaticchio", "Edward James", 170, 71, "R",
-            "R", "1897-09-04", "1910-09-15", "abbae101", "abbated01");
-    Player player2 = new Player("player2", 1877, 4, 15, "USA", "PA", "Latrobe", 1957,
-            1, 6, "USA", "FL", "Fort Lauderdale",
-            "Ed", "Abbaticchio", "Edward James", 170, 71, "R",
-            "R", "1897-09-04", "1910-09-15", "abbae101", "abbated01");
+    private List<Player> configureMocks(int numberOfPlayers) throws Exception {
+        List<Player> playersResponse = new ArrayList<>();
+        for (int i = 1; i <= numberOfPlayers; i++) {
+            String playerId = "player" + i;
+            int birthYear = 1877;
+            int birthMonth = 4;
+            int birthDay = 15;
+            String birthCountry = "USA";
+            String birthState = "PA";
+            String birthCity = "Latrobe";
+            int debutYear = 1957;
+            int debutMonth = 1;
+            int debutDay = 6;
+            String finalGameCountry = "USA";
+            String finalGameState = "FL";
+            String finalGameCity = "Fort Lauderdale";
+            String nameFirst = "Ed";
+            String nameLast = "Abbaticchio";
+            String nameGiven = "Edward James";
+            int height = 170;
+            int weight = 71;
+            String bats = "R";
+            String throwsHand = "R";
+            String debut = "1897-09-04";
+            String finalGame = "1910-09-15";
+            String retroID = "abbae101";
+            String bbrefID = "abbated01";
+
+            Player player = new Player(playerId, birthYear, birthMonth, birthDay, birthCountry, birthState, birthCity,
+                    debutYear, debutMonth, debutDay, finalGameCountry, finalGameState, finalGameCity,
+                    nameFirst, nameLast, nameGiven, height, weight, bats, throwsHand, debut, finalGame,
+                    retroID, bbrefID);
+            playersResponse.add(player);
+        }
+
+        Path path = Paths.get("/Users/aarvivo/Documents/player.csv");
+        List<Player> players = new ArrayList<>();
+        when(playerService.readCsvToBeanList(path, Player.class, players)).thenReturn(playersResponse);
+        return playersResponse;
+    }
+
 
     @Test
     void testGetPlayers() throws Exception {
-        Path path = Paths.get("/Users/aarvivo/Documents/player.csv");
-        List<Player> players = new ArrayList<>();
-        List<Player> playersResponse = new ArrayList<>();
-        playersResponse.add(player1);
-        playersResponse.add(player2);
-        when(playerService.readCsvToBeanList(path, Player.class, players)).thenReturn(playersResponse);
-
+        List<Player> players = configureMocks(3);
         List<Player> result = playersController.getPlayers();
 
-        assertEquals(2, result.size());
-        assertEquals(player1.getPlayerID(), result.get(0).getPlayerID());
-        assertEquals(player2.getPlayerID(), result.get(1).getPlayerID());
+        assertEquals(3, result.size());
+        assertEquals(players.get(0), result.get(0));
+        assertEquals(players.get(1), result.get(1));
+        assertEquals(players.get(2), result.get(2));
     }
 
     @Test
     void testGetPlayerById() throws Exception {
-        Path path = Paths.get("/Users/aarvivo/Documents/player.csv");
-        List<Player> players = new ArrayList<>();
-        List<Player> playersResponse = new ArrayList<>();
-        playersResponse.add(player1);
-        playersResponse.add(player2);
-        when(playerService.readCsvToBeanList(path, Player.class, players)).thenReturn(playersResponse);
+        List<Player> players = configureMocks(4);
+        Player result = playersController.getPlayerById(players.get(3).getPlayerID());
 
-        Player result = playersController.getPlayerById(player1.getPlayerID());
-
-        assertEquals(player1.getPlayerID(), result.getPlayerID());
+        assertEquals(players.get(3), result);
     }
 
     @Test
     void testGetPlayerByIdNotFound() throws Exception {
-        Path path = Paths.get("/Users/aarvivo/Documents/player.csv");
-        List<Player> players = new ArrayList<>();
-        players.add(player1);
-        players.add(player2);
-        when(playerService.readCsvToBeanList(path, Player.class, players)).thenReturn(players);
+        List<Player> players = configureMocks(4);
 
         ResponseStatusException exception = assertThrows(ResponseStatusException.class, () -> {
             playersController.getPlayerById("NotFound");
