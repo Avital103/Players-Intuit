e93b06c4364167c0b3fc432c61d14956fa2e1dc3
aarvivo <aarvivo@synamedia.com>
2024-03-16 21:12:45 +0200

    remove unused import

--- src/test/java/com/assignment/players/PlayersApplicationTests.java
+++ src/test/java/com/assignment/players/PlayersApplicationTests.java
@@ -15,7 +15,6 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
